c92f4db9e27274cb15a2028f29a3d609ebd1f9c8
racs182 <racs182@gmail.com>
2013-05-17 01:48:15 -0430

    Agregando maps y form en mod contacto

--- wp-content/themes/WP960gs-master/loop-contacto.php
+++ wp-content/themes/WP960gs-master/loop-contacto.php
@@ -30,11 +30,20 @@
 			<div class="row">
 					<div class="grid6">
 						<h4>Ubicacion</h4>
-							<img src="http://placehold.it/490x250" alt="">
+								<iframe width="400" height="230" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.ve/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=Caracas,+Venezuela&amp;aq=&amp;sll=10.468305,-66.879508&amp;sspn=0.196826,0.220757&amp;ie=UTF8&amp;hq=&amp;hnear=Municipio+Libertador+de+Caracas,+Distrito+Metropolitano+de+Caracas&amp;t=m&amp;ll=10.487812,-66.906738&amp;spn=4.320126,7.668457&amp;z=6&amp;iwloc=A&amp;output=embed"></iframe>
 					</div>
 					<div class="grid6">
 						<h4>Form</h4>
-							<img src="http://placehold.it/490x250" alt="">
+						<div class="formulario">
+							<form action="#" autocomplete="on">
+								<fieldset>
+									<input type="text" name="nombre" placeholder="Nombre" required></br>
+								<input type="text" name="email" placeholder="Email" required></br>
+							<textarea name="mensaje" id="" cols="60" rows="5" placeholder="Mensaje" required></textarea></br>
+						<button type="submit" class="btn enviar-btn">Enviar</button>
+					</fieldset>
+					</form>
+					</div>
 					</div>
 				</div>
 			</div>
